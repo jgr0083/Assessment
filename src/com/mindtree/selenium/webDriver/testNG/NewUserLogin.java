4cbce5972e06964f393b8600bfeb5881b40e9e9b
Jarell Reloj <jarellr@gmail.com>
2020-02-13 21:59:15 -0600

    New User Page Object Model

--- src/com/mindtree/selenium/webDriver/testNG/NewUserLogin.java
+++ src/com/mindtree/selenium/webDriver/testNG/NewUserLogin.java
@@ -18,6 +18,8 @@ import org.apache.log4j.PatternLayout;
 import com.mindtree.selenium.webDriver.basic.BrowserController;
 import com.mindtree.selenium.webDriver.basic.WindowController;
 import com.mindtree.selenium.webDriver.feedback.SendEmail;
+import com.mindtree.selenium.webDriver.pages.MenuBar;
+import com.mindtree.selenium.webDriver.pages.SignInPage;
 import com.mindtree.selenium.webDriver.resources.RandomGenerator;
 import com.mindtree.selenium.webDriver.resources.User;
 import com.mindtree.selenium.webDriver.utils.JDBCDriver;
@@ -54,11 +56,10 @@ public class NewUserLogin {
 
 	@Test(dependsOnMethods = { "openBrowser" })
 	public void createNewUser() throws FileNotFoundException, IOException {
+		MenuBar menu = new MenuBar();
+		SignInPage sign = new SignInPage();
 		JDBCDriver jdbc = new JDBCDriver();
 		Connection conn = jdbc.getConnection();
-		Properties p = new Properties();
-		FileInputStream file = new FileInputStream("..\\Assessment\\properties\\login.properties");
-		p.load(file);
 		RandomGenerator rando = new RandomGenerator();
 		String ranName = rando.randomString(10);
 		int phone = rando.randomInt(1000000000, 999999999);
@@ -76,28 +77,23 @@ public class NewUserLogin {
 		ctrl.max(driver);
 
 		logger.info("Open Sign in page");
-		ctrl.click(driver, By.xpath("//*[@id=\"navbarSupportedContent\"]/form/button[4]")); // Selects Sign in button
+		menu.signin(driver);
 		logger.info("Open Create Account Page");
-		ctrl.click(driver, By.cssSelector(".btn.btn-primary.btn-md")); // Clicks Create Account
+		sign.createAccount(driver);// Clicks Create Account
 
 		logger.info("Setting user information");
-		ctrl.type(driver, By.id(p.getProperty("username")), "Test Name" + ranName);
-		ctrl.type(driver, By.id(p.getProperty("phone")), phoneString);
+		
+		sign.setUsername(driver, ranName);		
+		sign.setPhone(driver, phoneString);
+		sign.setEmail(driver, ranName);
+		sign.setPassword(driver, "Password123!");
 
-		List<WebElement> email = driver.findElements(By.id(p.getProperty("email")));
-		email.get(1).sendKeys("Test_Name" + ranName + "@Test.com");
-
-		List<WebElement> password = driver.findElements(By.id(p.getProperty("password")));
-		password.get(1).sendKeys("Password123!");
-
+		
 		logger.info("Submitting new user");
-		List<WebElement> submit = driver.findElements(By.cssSelector(".btn.btn-primary.btn-lg.btn-block"));
-		submit.get(1).click();
+		sign.submit(driver);
 
 		logger.info("Closing new user setup");
-		List<WebElement> close = driver
-				.findElements(By.cssSelector(".mat-icon.notranslate.material-icons.mat-icon-no-color"));
-		close.get(11).click();
+		sign.close(driver);
 		
 		ctrl.pause(2000);
 		
@@ -106,41 +102,31 @@ public class NewUserLogin {
 		if(userId==0) {
 			System.out.println("ERROR: User not found in DB");
 			logger.error("User "+user.getUsername() +" not found in DB");
-			System.exit(0);
+		//	System.exit(0);
 		}else
 		{
 			logger.info("userId ID found: " + userId);
 			user.setUserID(userId);
 		}
-
-		// SELECT * FROM profile where user_id=216;
-
-		// role="alertdialog"
 	}
 	
-//Page object model for Selenium ***
+	
 	@Test(dependsOnMethods = { "openBrowser", "createNewUser" })
 	public void login() throws FileNotFoundException, IOException {
+		SignInPage sign = new SignInPage();
 		Properties p = new Properties();
 		FileInputStream file = new FileInputStream("..\\Assessment\\properties\\login.properties");
 		p.load(file);
 
 		logger.info("Logging in as new user");
-		List<WebElement> email = driver.findElements(By.id(p.getProperty("email")));
-		email.get(0).sendKeys(user.getEmail());
-
-		List<WebElement> password = driver.findElements(By.id(p.getProperty("password")));
-		password.get(0).sendKeys(user.getPassword());
-
-		List<WebElement> submit = driver.findElements(By.cssSelector(".btn.btn-primary.btn-lg.btn-block"));
-		submit.get(0).click();
+		sign.login(driver, user);
 	}
 
 	@AfterTest
 	public void close() {
 		email.send("NewUserLogin");
 		logger.info("Test Complete");
-		driver.close();
+		//driver.close();
 	}
 
 	//@Ignore
